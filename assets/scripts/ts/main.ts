f7d0920fbf8fca59b5225c4e87f32cea9183a435
Enrique Balleste <eballeste@divisionof.com>
2016-08-07 05:14:13 -0400

    transpile to es5
    es6 to es5 for broader browser support

--- assets/scripts/ts/main.ts
+++ assets/scripts/ts/main.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../typings/globals/core-js/index.d.ts" />
+
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { appRouterProviders } from './app.routes';
