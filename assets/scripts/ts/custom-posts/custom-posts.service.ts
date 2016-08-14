fd89497831ea5a70285aab4a54275ed905da5069
Enrique Balleste <eballeste@divisionof.com>
2016-08-14 02:33:25 -0400

    replace inline url with global const
    todo: setup config file with environment based variables (dev/staging/prod)

--- assets/scripts/ts/custom-posts/custom-posts.service.ts
+++ assets/scripts/ts/custom-posts/custom-posts.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { WP_API_URL } from '../services/data-access.service';
 
 @Injectable()
 export class CustomPostsService {
   constructor(private http: Http) { }
 
-  private _wpBase = 'http://ng.dev/wp-json/wp/v2/';
+  private _wpBase: string = WP_API_URL;
 
   getPosts() {
     return this.http.get(this._wpBase + 'custom_posts');
