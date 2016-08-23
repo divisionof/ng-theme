1e9fbccfc3bfe30ac4b9d321f19ce654a44365f1
deronjohnson <johdero@gmail.com>
2016-08-23 14:22:31 -0400

    add new constant for dist image path

--- assets/scripts/ts/services/data-access.service.ts
+++ assets/scripts/ts/services/data-access.service.ts
@@ -1,1 +1,4 @@
-export const WP_API_URL: string = "http://ngtheme.dev/wp-json/wp/v2/";
+let BASE_URL = "http://ng.dev";
+
+export const WP_API_URL: string = BASE_URL+"/wp-json/wp/v2/";
+export const IMAGE_PATH: string = BASE_URL+"/wp-content/themes/ng-theme/dist/images/"
